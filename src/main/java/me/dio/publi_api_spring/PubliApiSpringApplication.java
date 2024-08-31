c0ea7c0f84bf26e5fbca786daa4d6377aa7f4397
Diego Migueis <diegomigueis@gmail.com>
2024-08-31 15:27:47 -0300

    Initial commit

---
+++ src/main/java/me/dio/publi_api_spring/PubliApiSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.publi_api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubliApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubliApiSpringApplication.class, args);
+	}
+
+}
